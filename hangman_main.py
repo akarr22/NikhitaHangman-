b1ae2773c8a99f0035db5e9cff38e98b25ff0a05
akarr22 <60932509+akarr22@users.noreply.github.com>
2020-02-25 10:37:05 -0500

    Add files via upload

---
+++ hangman_main.py
@@ -0,0 +1,44 @@
+word = ("Anir")
+wordList = list(word.lower())
+updatedSpaces = []
+wordLen = len(word)
+lives = 5
+letter = " "
+
+for i in range (0, int(wordLen)):
+    updatedSpaces.append("_")
+    
+def getLetter():
+    global letter
+    letter = raw_input ("Enter a letter guess    ")
+
+def check():
+    global updatedSpaces
+    global lives
+    global letter
+    for x in range(0, int(wordLen)):
+        if letter == wordList[x]:
+            updatedSpaces[x] = wordList[x]
+            print(updatedSpaces)
+            print("You have:   ", lives, "   lives left!")
+            checklist = "".join(updatedSpaces)
+            master = "".join(wordList)
+            if checklist == master:
+                print("Congrats you solved the word!    ")
+                #break
+            else:
+                getLetter()
+        else:
+            lives -= 1
+            if lives != 0:
+                print("You have:  " + str(lives) + "  lives left!")
+                print(updatedSpaces)
+                getLetter()
+            else:
+                print("Game Over   ")
+                
+def game():
+    getLetter()
+    check()
+    
+game()                      
