b09770a6341997c73bf8eabf66dd4526caef8fc8
akarr22 <60932509+akarr22@users.noreply.github.com>
2020-02-26 09:59:07 -0500

    finished hangman

--- hangman_main.py
+++ hangman_main.py
@@ -1,9 +1,15 @@
-word = ("Anir")
+word = raw_input("Type a word   ")
 wordList = list(word.lower())
 updatedSpaces = []
 wordLen = len(word)
 lives = 5
 letter = " "
+x = 0
+print("\x1b[2J")
+#print("\x1b[5;3H")
+
+for i in range (0,100):
+    print
 
 for i in range (0, int(wordLen)):
     updatedSpaces.append("_")
@@ -16,26 +22,34 @@ def check():
     global updatedSpaces
     global lives
     global letter
-    for x in range(0, int(wordLen)):
-        if letter == wordList[x]:
-            updatedSpaces[x] = wordList[x]
-            print(updatedSpaces)
-            print("You have:   ", lives, "   lives left!")
-            checklist = "".join(updatedSpaces)
-            master = "".join(wordList)
-            if checklist == master:
-                print("Congrats you solved the word!    ")
-                #break
-            else:
-                getLetter()
-        else:
-            lives -= 1
-            if lives != 0:
-                print("You have:  " + str(lives) + "  lives left!")
+    global x
+    while updatedSpaces != wordList:
+        for x in range(0, int(wordLen)):
+            if letter == wordList[x]:
+                updatedSpaces[x] = wordList[x]
                 print(updatedSpaces)
-                getLetter()
+                print("You have:   ", lives, "   lives left!")
+                checklist = "".join(updatedSpaces)
+                master = "".join(wordList)
+                if checklist == master:
+                    print("Congrats you solved the word!    ")
+                    #break
+                else:
+                    getLetter()
             else:
-                print("Game Over   ")
+                if lives == 0:
+                    print("Game Over   ")
+                    # break
+                else:
+                    print("You have:  " + str(lives) + "  lives left!")
+                    print(updatedSpaces)
+                    getLetter()
+                lives -= 1
+        if lives == -1:
+            break
+        x += 1
+    
+      
                 
 def game():
     getLetter()
