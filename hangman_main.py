ac5798bf4ec7c8a18f90276bd996acbd1bdc8e30
akarr22 <60932509+akarr22@users.noreply.github.com>
2020-02-27 10:08:49 -0500

    probably the final
    if a letter appears up to 3 times in a word, all three show up with one guess

--- hangman_main.py
+++ hangman_main.py
@@ -6,11 +6,20 @@ lives = 5
 letter = " "
 x = 0
 letterIndex = 0
+letterIndex2 = 0
+letterIndex3 = 0
 inWord = False
+inWord2 = False
+inWord3 = False
+
+for i in range(0,100):
+    print(" ")
 
 for i in range (0, int(wordLen)):
     updatedSpaces.append("_")
     
+print(updatedSpaces)
+
 def getLetter():
     global letter
     letter = raw_input ("Enter a letter guess    ")
@@ -20,8 +29,13 @@ def check():
     global lives
     global letter
     global x
-    global inWord
     global letterIndex
+    global letterIndex2
+    global letterIndex3
+    global inWord
+    global inWord2
+    global inWord3
+
     while updatedSpaces != wordList:
         for i in range(0, int(wordLen)):
             if letter == wordList[i]:
@@ -30,25 +44,52 @@ def check():
                 break
             else:
                 inWord = False
+        for i in range(0, int(wordLen)):
+            if (letter == wordList[i]) and (i != letterIndex):
+                letterIndex2 = i
+                inWord2 = True
+                break
+            else:
+                inWord2 = False 
+        for i in range(0, int(wordLen)):
+            if (letter == wordList[i]) and (i != letterIndex) and (i != letterIndex2):
+                letterIndex3 = i
+                inWord3 = True
+                break
+            else:
+                inWord3 = False 
         if inWord:
             updatedSpaces[letterIndex] = wordList[letterIndex]
-            print(updatedSpaces)
-            print("You have: " + str(lives) + " lives left!")
             checklist = "".join(updatedSpaces)
             master = "".join(wordList)
             if checklist == master:
                 print("Congrats you solved the word!    ")
                 break
-        else:
+        if inWord2:
+            updatedSpaces[letterIndex2] = wordList[letterIndex2]
+            checklist = "".join(updatedSpaces)
+            master = "".join(wordList)
+            if checklist == master:
+                print("Congrats you solved the word!    ")
+                break
+        if inWord3:
+            updatedSpaces[letterIndex3] = wordList[letterIndex3]
+            checklist = "".join(updatedSpaces)
+            master = "".join(wordList)
+            if checklist == master:
+                print("Congrats you solved the word!    ")
+                break
+        if (inWord == False) and (inWord2 == False):
             lives -= 1
             if lives == 0:
                 print("Game Over   ")
                 break
-            else:
-                print("You have:  " + str(lives) + "  lives left!")
-                print(updatedSpaces)
+        print(updatedSpaces)
+        print("You have: " + str(lives) + " lives left!")
         getLetter()
         x += 1
+    
+    
     
       
                 
